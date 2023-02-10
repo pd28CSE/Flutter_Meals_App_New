8d72a8e1ec48b8161e4820b6f6bc7ff34af22772
Partho Debnath <parthodebnath28@gmail.com>
2023-02-10 16:00:10 +0600

    add main_drawer Drawer and connect to tabs_screen

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  Widget buildFiter(IconData icon, String title, VoidCallback pressed) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: pressed,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            width: double.infinity,
+            height: 120,
+            decoration: BoxDecoration(
+              color: Theme.of(context).accentColor,
+            ),
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          buildFiter(Icons.restaurant, 'Restaurant', () {
+            print('Restaurant');
+          }),
+          buildFiter(Icons.settings, 'Filter', () {
+            print('Filter');
+          }),
+        ],
+      ),
+    );
+  }
+}
