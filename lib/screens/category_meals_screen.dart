5873847c6d8abad09948ad28b718662eff35370e
Partho Debnath <parthodebnath28@gmail.com>
2023-02-10 21:59:40 +0600

    add some important statement in category_meals_screen for delete selected meal.

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,45 +1,68 @@
 import 'package:flutter/material.dart';
 
 import '../models/dummy_data.dart';
+import '../models/meal.dart';
 import '../widgets/meal_item.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const String routeName = 'meals-category';
-  // final String categoryId;
-  // final String categoryTitle;
 
-  // const CategoryMealsScreen({
-  //   super.key,
-  //   required this.categoryId,
-  //   required this.categoryTitle,
-  // });
+  @override
+  State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  // final String categoryId;
+  String? categoryTitle;
+  List<Meal> displayedMeals = [];
+
+  @override
+  void initState() {
+    print('------init State');
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('------did Change Dependencies');
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final String categoryId = routeArgs['id'] as String;
+    categoryTitle = routeArgs['title'] as String;
+    displayedMeals = DUMMY_MEALS.where((item) {
+      return item.categories.contains(categoryId);
+    }).toList();
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('------dispose');
+    super.dispose();
+  }
 
   void deleteMealOnPressed(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
     print('remove -> $mealId');
   }
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs =
-        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
-    final String categoryId = routeArgs['id'] as String;
-    final String categoryTitle = routeArgs['title'] as String;
-    final categoryMeals = DUMMY_MEALS.where((item) {
-      return item.categories.contains(categoryId);
-    }).toList();
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle),
+        title: Text(categoryTitle as String),
       ),
       body: ListView.builder(
         itemBuilder: (cntxt, index) {
           return MealItem(
-            id: categoryMeals[index].id,
+            id: displayedMeals[index].id,
             deleteMeal: deleteMealOnPressed,
           );
         },
-        itemCount: categoryMeals.length,
+        itemCount: displayedMeals.length,
       ),
     );
   }
